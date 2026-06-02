f2113bd66607c952d03b9957e93bbae293346175
Giovanna Mafra <gio.marques.m@gmail.com>
2026-06-02 16:37:39 -0300

    script_treino

---
+++ script_treinamento.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=train-rf-can
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=16G
+#SBATCH -p short-simple
+#SBATCH --qos=simple
+#SBATCH --output=slurm_train_rf_%j.out 
+#SBATCH --signal=USR1@5
+
+echo ">> Verificando/Garantindo que o scikit-learn está instalado..."
+python3 -m pip install --user --upgrade scikit-learn "numpy<2.0.0" "pandas<2.0.0"
+
+echo ">> Entrando na pasta do framework..."
+cd /home/CIN/gmm8/automotive-ids-evaluation-framework-deteccao_intrusao
+
+# Defina aqui o caminho do JSON que você salvou com as configurações do Random Forest
+SELECTED_MODEL_TRAIN_VALIDATE_CONFIG="config_jsons/model_train_validate/CAN_rf_train_one_class.json"
+
+echo ">> Iniciando o Treinamento do Random Forest (One-Class) para o CAN..."
+python3 execute_model_train_validation.py --config $SELECTED_MODEL_TRAIN_VALIDATE_CONFIG
+
+echo ">> Treinamento concluído!"
