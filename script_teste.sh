a759c5b7decc0582ce857906720212f5aba7e231
Giovanna Mafra <gio.marques.m@gmail.com>
2026-06-05 18:00:25 -0300

    testando cnn para o teste 3

--- script_teste.sh
+++ script_teste.sh
@@ -15,7 +15,7 @@ echo ">> Entrando na pasta do framework..."
 cd /home/CIN/gmm8/automotive-ids-evaluation-framework-deteccao_intrusao
 
 # Defina aqui o caminho do JSON que você salvou com as configurações do Random Forest
-SELECTED_MODEL_TRAIN_VALIDATE_CONFIG="config_jsons/model_test/CAN_test_01_PrunedCNNIDS.json"
+SELECTED_MODEL_TRAIN_VALIDATE_CONFIG="config_jsons/model_test/CAN_test_03_PrunedCNNIDS.json"
 
 echo ">> Iniciando o Teste do CNN para o CAN Teste 01..."
 python3 execute_model_test.py --model_test_config $SELECTED_MODEL_TRAIN_VALIDATE_CONFIG
